e2ab86526a473125d21729c287e55a2fe2b1ffd0
nkalaitzian <nikoskalai@gmail.com>
2024-08-22 19:35:39 +0300

    Ensure that there are no duplicate RNGs

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,26 +1,29 @@
 // This is where it all goes :)
 
-window.onload = function(){
+window.onload = function () {
   init();
 };
 
-titleParagraph = null;
-rngText = null;
-spotButtons = [];
-maxValueFilled = '-1';
-minValueFilled = '1001';
-score = 0;
+let titleParagraph = null;
+let rngText = null;
+let spotButtons = [];
+let maxValueFilled = "-1";
+let minValueFilled = "1001";
+let assignedNumbers = [];
+let score = 0;
 
 function init() {
-  rngText = document.getElementById('rngText');
-  titleParagraph = document.getElementById('titleParagraph');
-  titleParagraph.innerHTML = 'Numbers Game';
-  spotButtons = Array.prototype.slice.call(document.getElementsByClassName('spotButton'));
+  rngText = document.getElementById("rngText");
+  titleParagraph = document.getElementById("titleParagraph");
+  titleParagraph.innerHTML = "Numbers Game";
+  spotButtons = Array.prototype.slice.call(
+    document.getElementsByClassName("spotButton"),
+  );
   spotButtons.forEach((spotButton) => {
     spotButton.addEventListener("click", (sB) => assignNumber(sB));
   });
   toggleSpotButtons(false);
-  document.getElementById('newGameButton').addEventListener('click', newGame);
+  document.getElementById("newGameButton").addEventListener("click", newGame);
 }
 
 function toggleSpotButtons(toggleState) {
@@ -60,22 +63,24 @@ function enableSpotButtons(rngValue) {
   //   toggleSpotButtons(false);
   //   return;
   // }
-  eligibleButtons.forEach((spotButton) => { enableSpotButton(spotButton) });
+  eligibleButtons.forEach((spotButton) => {
+    enableSpotButton(spotButton);
+  });
 }
 
 function buttonSpotText(spotButton) {
   if (spotButton == null) {
-    return '';
+    return "";
   }
-  targetTextId = spotButton.dataset['textTarget'];
+  targetTextId = spotButton.dataset["textTarget"];
   textTarget = document.getElementById(targetTextId);
   return textTarget.innerHTML;
 }
 
 function enableSpotButton(spotButton) {
-  targetTextId = spotButton.dataset['textTarget'];
+  targetTextId = spotButton.dataset["textTarget"];
   textTarget = document.getElementById(targetTextId);
-  if (textTarget.innerHTML == '') {
+  if (textTarget.innerHTML == "") {
     spotButton.disabled = false;
   }
 }
@@ -84,16 +89,25 @@ function disableSpotButton(spotButton) {
   spotButton.disabled = true;
 }
 
+function rng() {
+  newRng = Math.floor(Math.random() * 1000) + 1 + "";
+  while (assignedNumbers.includes(newRng)) {
+    newRng = Math.floor(Math.random() * 1000) + 1 + "";
+  }
+  return newRng;
+}
+
 function generateRng() {
-  rngValue = Math.floor(Math.random() * 1000) + 1;
+  rngValue = rng();
   rngText.innerHTML = rngValue;
   enableSpotButtons(rngValue);
 }
 
 function assignNumber(spotButton) {
-  targetTextId = spotButton.target.dataset['textTarget'];
+  targetTextId = spotButton.target.dataset["textTarget"];
   textTarget = document.getElementById(targetTextId);
   textTarget.innerHTML = rngText.innerHTML;
+  assignedNumbers.push(rngText.innerHTML);
   toggleSpotButtons(false);
   spotButton.target.classList.add("hidden");
   if (rngText.innerHTML > maxValueFilled) {
@@ -108,22 +122,23 @@ function assignNumber(spotButton) {
 
 function updateScore(newScore) {
   score = newScore;
-  scoreLabel = document.getElementById('scoreLabel');
-  scoreLabel.innerHTML = 'Score <strong>' + score + '</strong>/20';
+  scoreLabel = document.getElementById("scoreLabel");
+  scoreLabel.innerHTML = "Score <strong>" + score + "</strong>/20";
 }
 
 function newGame() {
   toggleSpotButtons(false);
   spotButtons.forEach((spotButton) => {
-    spotButton.classList.remove('hidden');
-    targetTextId = spotButton.dataset['textTarget'];
+    spotButton.classList.remove("hidden");
+    targetTextId = spotButton.dataset["textTarget"];
     textTarget = document.getElementById(targetTextId);
-    textTarget.innerHTML = '';
+    textTarget.innerHTML = "";
   });
-  rngText.innerHTML = '';
-  maxValueFilled = '-1';
-  minValueFilled = '1001';
-  titleParagraph.innerHTML = 'Numbers Game';
+  rngText.innerHTML = "";
+  maxValueFilled = "-1";
+  minValueFilled = "1001";
+  titleParagraph.innerHTML = "Numbers Game";
+  assignedNumbers = [];
   generateRng();
   updateScore(0);
 }
